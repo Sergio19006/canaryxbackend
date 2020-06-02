c0ae2a81702cb7c2c9519f94e00eae0c74ea8aee
Sergio <sergiodphn@gmail.com>
2020-06-02 01:00:05 +0100

    fail

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ const server = app.listen(app.get("port"), () => {
     );
     console.log("  Press CTRL-C to stop process\n");
     console.log("pass:", process.env.ATLAS_PASSWORD);
+    console.log("pass2:", process.env.MAIL_PASSWORD);
 });
 
 export default server;
