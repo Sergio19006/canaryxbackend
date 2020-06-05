9da822534ce03ad92db2ab61cdaf8427d0e3146c
Sergio del Pino Hernández <44278079+Sergio19006@users.noreply.github.com>
2020-06-05 10:45:48 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(fileUpload());
 app.use((req: Request, res: Response, next: any) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.append('Access-Control-Allow-Headers', ['*']);
     next();
 });
 
